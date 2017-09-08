10afe6f8db326f62692dab1e52556c84ec4f9b58
AH7 <assaf.he2@gmail.com>
2017-09-08 11:15:23 +0100

    feat: cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,48 +16,9 @@ if [ $? -ne "0" ]; then
   exit 2
 fi
 
-useradd -m -u $USER_ID -o -d /home/node docker-user
-
-TEST_SETTINGS="";
-if [ ! -z $TARGET_FILE ]; then
-  TEST_SETTINGS="--file=${TARGET_FILE} "
+if [ -z $USER_ID ]; then
+  USER_ID=`id -u`
 fi
 
-if [ ! -z $ORGANIZATION ]; then
-  TEST_SETTINGS="${TEST_SETTINGS} --org=${ORGANIZATION}"
-fi
-#
-# if [ -d /home/node/.m2/repository ]; then
-#     # Add .m2 path position - because we run as a different user
-#     if [ ! -z $ENV_FLAGS ]; then
-#       # ENV_FLAGS="-Dmaven.repo.local=/home/node/.m2/repository ${ENV_FLAGS}"
-#     else
-#       # ENV_FLAGS="-Dmaven.repo.local=/home/node/.m2/repository"
-#     fi
-# fi
-
-if [ ! -z $ENV_FLAGS ]; then
-  TEST_SETTINGS="${TEST_SETTINGS} -- ${ENV_FLAGS}"
-fi
-
-export JAVA_OPTS="-Xms512m -Xmx512m -XX:ReservedCodeCacheSize=128m"
-su docker-user -m -c "cd $PROJECT_PATH/$PROJECT_FOLDER && snyk test --json $TEST_SETTINGS > res.json"
-RC=$?
-cd $PROJECT_PATH/$PROJECT_FOLDER
-cat res.json
-
-if [ $RC -ne "0" ] && [ $RC -ne "1" ]; then
-  echo $RC
-  exit 3;
-fi
-
-cat res.json | jq '.vulnerabilities|= map(. + {severity_numeric: (if(.severity) == "high" then 1 else (if(.severity) == "medium" then 2 else (if(.severity) == "low" then 3 else 4 end) end) end)}) |.vulnerabilities |= sort_by(.severity_numeric) | del(.vulnerabilities[].severity_numeric)' | snyk-to-html | sed 's/<\/head>/  <link rel="stylesheet" href="snyk_report.css"><\/head>/' > snyk_report.html
-cat /home/node/snyk_report.css > snyk_report.css
-# check monitor
-if [ ! -z $MONITOR ]; then
-  su docker-user -m -c "cd $PROJECT_PATH/$PROJECT_FOLDER && snyk monitor $TEST_SETTINGS"
-fi
-
-rm res.json
-
-exit $RC
+useradd -m -o -u $USER_ID -d /home/node docker-user 2>/dev/null
+su docker-user -m -c "PATH=$PATH /home/node/run-snyk.sh"
